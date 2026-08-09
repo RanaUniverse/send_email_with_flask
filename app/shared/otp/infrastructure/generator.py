3aa852d17c5256e5b9cb564a0aa8afefc79d5586
RanaUniverse <RanaUniverse321@gmail.com>
2026-08-09 21:03:15 +0530

    opt generate now got more secure

--- app/shared/otp/infrastructure/generator.py
+++ app/shared/otp/infrastructure/generator.py
@@ -5,7 +5,7 @@ Here i will have the logic to generate the otp
 this values i will call in my send, executes and so on using the factory.py
 """
 
-import random
+import secrets
 import string
 
 
@@ -13,31 +13,49 @@ from ..interfaces.generator import OTPGenerator  # type: ignore
 
 
 class LocalTestingOTPGenerator:
+    """
+    For LOcal testing i mostly not use htis
+    """
+
     def generate(
         self,
         length: int,
     ) -> str:
-        return "111111"
+        if length <= 0:
+            raise ValueError("OTP length must be greater than zero.")
+        return "123456"
 
 
 class OTPNumberGenerator:
+    """
+    This cretes the numeric otp of some length
+    """
+
     def generate(
         self,
         length: int,
     ) -> str:
         # i will use another login in reality #TODO
-        n = random.randint(
-            a=100000,
-            b=999999999999999999999999999999999999,
-        )
-        return str(n)[0:length]
+        if length < 0:
+            raise ValueError("Otp should must be greater than zero.")
+
+        digits = string.digits
+        otp = "".join(secrets.choice(digits) for _ in range(length))
+        return otp
 
 
 class OTPAlphabetGenerator:
+    """
+    This will generate the ALPHABETICAL OTP
+    """
+
     def generate(
         self,
         length: int,
     ) -> str:
+        if length < 0:
+            raise ValueError("Otp should must be greater than zero.")
+
         LETTERS = string.ascii_uppercase
-        s = "".join(random.choice(LETTERS) for _ in range(length))
-        return s
+        otp = "".join(secrets.choice(LETTERS) for _ in range(length))
+        return otp

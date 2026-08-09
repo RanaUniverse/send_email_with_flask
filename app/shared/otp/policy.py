84f8227f4c18b97e443f4935892351b0a6b01ab0
RanaUniverse <RanaUniverse321@gmail.com>
2026-08-09 17:08:01 +0530

    added new policy so that by this it will know if to use the login, register and their configuration settings

---
+++ app/shared/otp/policy.py
@@ -0,0 +1,49 @@
+"""
+app/shared/otp/policy.py
+
+Security realted thigns will be present here
+This will keep  the otp policy which will decide how
+and what the otp generation will be and how those will be
+
+This is for separate the validity, cooldown and so on for different
+purpose of differne otp send type.
+"""
+
+from .enums import OTPPurpose
+from .models import OTPPolicy
+
+register_policy = OTPPolicy(
+    validity=60,
+    cooldown=60,
+    max_attempts=5,
+    length=4,
+)
+
+login_policy = OTPPolicy(
+    validity=120,
+    cooldown=30,
+    max_attempts=5,
+    length=6,
+)
+
+forget_password_policy = OTPPolicy(
+    validity=300,
+    cooldown=60,
+    max_attempts=5,
+    length=8,
+)
+
+
+OTP_POLICY_MAP = {
+    OTPPurpose.REGISTER: register_policy,
+    OTPPurpose.LOGIN: login_policy,
+    OTPPurpose.FORGET_PASSWORD: forget_password_policy,
+}
+
+
+def get_otp_policy_obj(
+    purpose: OTPPurpose,
+) -> OTPPolicy:
+
+    x = OTP_POLICY_MAP[purpose]
+    return x

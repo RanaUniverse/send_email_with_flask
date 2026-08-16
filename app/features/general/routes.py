c14fdbfc9fbf79630f0ba0da49e3d35881218286
RanaUniverse <RanaUniverse321@gmail.com>
2026-08-16 19:38:17 +0530

    now showing session informaiton in the index home page

--- app/features/general/routes.py
+++ app/features/general/routes.py
@@ -5,10 +5,7 @@ Here i will make normal routes of what my general
 and some common things
 """
 
-from flask import (
-    Blueprint,
-    render_template,
-)
+from flask import flash, Blueprint, render_template, session
 
 general_bp = Blueprint(
     name="general_bp",
@@ -19,6 +16,15 @@ general_bp = Blueprint(
 
 @general_bp.route(rule="/")
 def home_page():
+    all_data = dict(session)
+    flash(
+        "Development Things",
+        "success",
+    )
+    flash(
+        str(all_data),
+        category="success",
+    )
     return render_template(
         template_name_or_list="general/index.html",
     )

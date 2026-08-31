ac553637386ebcaa8c3aefd325578dc313310e4c
RanaUniverse <RanaUniverse321@gmail.com>
2026-08-31 15:46:48 +0530

    make some improvement on the login system with resend functionality and using js and change little session fun here

--- app/features/identity/presentation/routes.py
+++ app/features/identity/presentation/routes.py
@@ -10,7 +10,6 @@ from flask import (
     render_template,
     redirect,
     url_for,
-    session,
 )
 
 
@@ -32,6 +31,14 @@ from ..presentation.authentication import FlaskLoginUser
 from app.shared.otp.policy import get_otp_policy_obj
 from app.shared.otp.enums import OTPPurpose
 
+
+from app.shared.session.enums import IdentitySessionKey
+from app.shared.session.service import (
+    set_identity_key as set_identity_key_in_session,
+    pop_key as pop_key_from_session,
+    get_identity_email as get_identity_email_from_session,
+)
+
 # TODO  i will later change this to call the service which will call the otp
 
 
@@ -39,6 +46,7 @@ auth_bp = Blueprint(
     name="auth_bp",
     import_name=__name__,
     template_folder="templates",
+    # static_folder="static",
 )
 
 
@@ -113,7 +121,9 @@ def login_with_password(
     I will make this workable with login routes goodly and logically
     """
 
-    email = session.get("pending_login_email")
+    email = get_identity_email_from_session(
+        IdentitySessionKey.LOGIN_PENDING,
+    )
 
     if email is None:
         flash(
@@ -136,10 +146,11 @@ def login_with_password(
         )
 
         if user_obj:
-            login_user(user=user_obj)
-            session.pop(
-                "pending_login_email",
-                None,
+            login_user(
+                user=user_obj,
+            )
+            pop_key_from_session(
+                key=IdentitySessionKey.LOGIN_PENDING,
             )
             flash(
                 "Login Successfull",
@@ -209,8 +220,10 @@ def register(
 
         if register.next_step == AfterRegistrationNextStep.VERIFY_OTP:
 
-            session["pending_email"] = form.email.data
-            # form.here i will chagne to number =4 or what
+            set_identity_key_in_session(
+                key=IdentitySessionKey.REGISTER_PENDING,
+                email_value=form.email.data or "",
+            )
 
             return redirect(
                 url_for(
@@ -219,7 +232,9 @@ def register(
             )
 
         if register.next_step == AfterRegistrationNextStep.ENTER_PASSWORD:
-            session["pending_login_email"] = form.email.data
+            set_identity_key_in_session(
+                key=IdentitySessionKey.LOGIN_PENDING,
+            )
             return redirect(
                 url_for(
                     "auth_bp.login_with_password",
@@ -241,6 +256,20 @@ def register(
                     message=f"{field.upper()}: {error}",
                     category="danger",
                 )
+    # Now here get request is working
+    pending_email = get_identity_email_from_session(
+        IdentitySessionKey.REGISTER_PENDING,
+    )
+
+    if pending_email is not None:
+        # later i will add redis checking if possible #TODO
+        flash(
+            f"📧 Registration pending for {pending_email}. "
+            "🔐 Verify the OTP to continue, or 🔄 use a different email.",
+            "warning",
+        )
+
+        return redirect(url_for("auth_bp.verify_otp"))
 
     return render_template(
         template_name_or_list="auth/register.html",
@@ -255,12 +284,18 @@ def register(
 def verify_otp(
     register_service: RegistrationServiceDep,
 ):
+    """
+    As this function should to run after the registration has done
+    so i need to check if user comes externally or not also
+    """
 
-    email = session.get("pending_email")
+    email = get_identity_email_from_session(
+        key=IdentitySessionKey.REGISTER_PENDING,
+    )
 
     if email is None:
         flash(
-            message="First Register Your Account with Email ID",
+            message="First Register Your Account with Email ID then verify your accoutn",
             category=FlashCategory.WARNING,
         )
         return redirect(
@@ -320,7 +355,7 @@ def verify_otp(
                 "success",
             )
 
-            session.pop("pending_email", None)
+            pop_key_from_session(key=IdentitySessionKey.REGISTER_PENDING)
 
             return redirect(
                 url_for(
@@ -337,4 +372,46 @@ def verify_otp(
         "auth/verify_otp.html",
         form=form,
         email=email,
+    )
+
+
+@auth_bp.route(
+    rule="/change-registration-email",
+    methods=[
+        "POST",
+    ],
+)
+def change_registration_email():
+    """
+    When this request will come i need to check if the user
+    """
+    pop_key_from_session(key=IdentitySessionKey.REGISTER_PENDING)
+
+    flash(
+        message="Please enter your new email address and continue the registration.",
+        category=FlashCategory.INFO,
+    )
+
+    return redirect(url_for("auth_bp.register"))
+
+
+@auth_bp.route(rule="/resend-registration-otp", methods=["POST"])
+def resend_registration_otp(
+    register_service: RegistrationServiceDep,
+):
+    """
+    Here i need to decide if the otp sending will done now or not
+    then only i will send the otp.
+    """
+    # TODO: implement OTP resend logic
+
+    flash(
+        message="📨 A new OTP will be sent here.",
+        category=FlashCategory.INFO,
+    )
+
+    return redirect(
+        url_for(
+            "auth_bp.verify_otp",
+        )
     )
